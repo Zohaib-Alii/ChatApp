7a6a7997679a4ad00d8dcb176707dcc545e669c2
Zohaib Ali <Zobiiiarain803@gmail.com>
2022-09-16 08:52:00 +0100

    adding sorting in array functionality (9-16-22)

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 
 const Search = () => {
+  // var Arr = [1, 7, 2, 8, 3, 4, 5, 0, 9];
+  var Arr = [3, 8, 6, 1, 0];
+  const sortingg = () => {
+    for (let i = 1; i < Arr.length; i++) {
+      debugger;
+      for (let j = 0; j < i; j++) {
+        debugger;
+        if (Arr[i] < Arr[j]) {
+          debugger;
+          var x = Arr[i];
+          Arr[i] = Arr[j];
+          Arr[j] = x;
+        }
+      }
+    }
+
+    console.log(Arr);
+  };
+  const minNumr = () => {
+    let leastNo = Arr[0];
+    for (let i = 0; i < Arr.length; i++) {
+      if (leastNo > Arr[i]) {
+        leastNo = Arr[i];
+      }
+
+      console.log(leastNo, "temp");
+    }
+  };
+  const printThisPage = () => {
+    window.print();
+  };
   return (
     <div>
       {" "}
       <div className="search">
+        <button onClick={sortingg}> perform</button>
+        <button onClick={minNumr}>Least No print</button>
+        <button onClick={printThisPage}>print this page </button>
         <div className="searchForm">
           <input
             type="text"
